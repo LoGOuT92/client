c5cb11dda3cbd3f02d8071a670df652207a59a88
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-02-13 02:52:36 +0100

    created articules component

---
+++ src/components/Articules/Articules.tsx
@@ -0,0 +1,25 @@
+import styles from "./Articules.module.scss";
+import SingleArticule from "./SingleArticule/SingleArticule";
+
+export function Articules() {
+  return (
+    <section className={styles.articulesContainer}>
+      <h2>LATEST NEWS</h2>
+      <div className={styles.articules}>
+        <SingleArticule />
+        <SingleArticule />
+      </div>
+      <div className={styles.smallArticules}>
+        <SingleArticule smallArticule="test" />
+        <SingleArticule smallArticule="test" />
+        <SingleArticule smallArticule="test" />
+        <SingleArticule smallArticule="test" />
+        <SingleArticule smallArticule="test" />
+        <SingleArticule smallArticule="test" />
+        <SingleArticule smallArticule="test" />
+        <SingleArticule smallArticule="test" />
+        <SingleArticule smallArticule="test" />
+      </div>
+    </section>
+  );
+}
