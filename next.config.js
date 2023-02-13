a27e787239fe756912f5045c2329287081a036b3
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-02-13 02:50:53 +0100

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        port: "",
+        pathname: "/account123/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    loader: "default",
+    minimumCacheTTL: 60,
+    domains: [
+      "i.ibb.co",
+      "assets-eu-01.kc-usercontent.com",
+      "storage.googleapis.com",
+      "www.acmilan.com",
+    ],
+  },
+};
