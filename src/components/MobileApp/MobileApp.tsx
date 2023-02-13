e689e047971ac6ca550d0b353364a1ddd808d7e1
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-02-13 02:53:59 +0100

    created mobiel app component

---
+++ src/components/MobileApp/MobileApp.tsx
@@ -0,0 +1,29 @@
+import { PrimaryButton } from "../ui/PrimaryButton";
+import styles from "./MobileApp.module.scss";
+import Image from "next/image";
+
+export function MobileApp({
+  img,
+  title,
+  paragraph,
+  button,
+}: {
+  img: string;
+  title: string;
+  paragraph: string;
+  button: string;
+}) {
+  return (
+    <section className={styles.mobileAppContainer}>
+      <h2>{title}</h2>
+      <div style={{ marginTop: "30px", border: "2px solid white" }}>
+        <Image width={1140} height={445} alt="" src={img} />
+        <div className={styles.mobileAppContent}>
+          <h2>{title}</h2>
+          <p>{paragraph}</p>
+          <PrimaryButton title={button} color="red" />
+        </div>
+      </div>
+    </section>
+  );
+}
