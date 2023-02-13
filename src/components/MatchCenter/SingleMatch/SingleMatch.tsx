55dcd148329aed99411b03aa3f1df6962039f09c
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-02-13 02:53:27 +0100

    created single match component

---
+++ src/components/MatchCenter/SingleMatch/SingleMatch.tsx
@@ -0,0 +1,31 @@
+import styles from "./SingleMatch.module.scss";
+import Image from "next/image";
+export default function SingleMatch({ type }: { type: string }) {
+  return (
+    <div className={styles.SingleMatch}>
+      <h2>{type}</h2>
+      <div className={styles.SingleMatchContent}>
+        <div className={styles.SingleTeam}>
+          <Image
+            alt=""
+            src="https://storage.googleapis.com/static-production.netcosports.com/directv/OPTA_LOGOS/120.png"
+            width={90}
+            height={90}
+          />
+          <span>Milan</span>
+        </div>
+        <span>2-1</span>
+        <div className={styles.SingleTeam}>
+          <Image
+            alt=""
+            src="https://storage.googleapis.com/static-production.netcosports.com/directv/OPTA_LOGOS/121.png"
+            width={90}
+            height={90}
+          />
+          <span>Milan</span>
+        </div>
+      </div>
+      <span>2020-02-02</span>
+    </div>
+  );
+}
