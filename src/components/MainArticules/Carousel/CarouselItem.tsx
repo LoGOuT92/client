42db824a3d22b5bee226bde26a24f38bbdc4ba93
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-02-13 02:53:11 +0100

    created carousel item component

---
+++ src/components/MainArticules/Carousel/CarouselItem.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from "./carouselItem.module.scss";
+interface item {
+  title: string;
+  paragraph: string;
+  image: string;
+}
+
+export function CarouselItem({ item }: { item: item }) {
+  return (
+    <div className={styles.carouselItem}>
+      <img className={styles.carouselImg} src={item.image} alt="" />
+      <div className={styles.mainArticulesContent}>
+        <div>
+          <h2>{item.title}</h2>
+          <p>{item.paragraph}</p>
+        </div>
+        <div className={styles.mainArticulesContentLink}>
+          <a>READ MORE</a>
+        </div>
+      </div>
+    </div>
+  );
+}
