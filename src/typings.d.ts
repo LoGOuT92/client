1742e25c45c48cb5a466da458cda712842bcf1b2
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-02-14 01:10:25 +0100

    build: created types file

---
+++ src/typings.d.ts
@@ -0,0 +1,31 @@
+export interface User{
+    comments: Array;
+    createdAt: string;
+    email: string;
+    image: string;
+    isAdmin: boolean;
+    isModerator: boolean;
+    isBanned: boolean;
+    password: string;
+    updatedAt:string;
+    username: string;
+    _id:string;
+}
+export interface Ipost{
+    author:User;
+    comments: Comment[];
+    context:string;
+    header: string;
+    image: string;
+    slug: string;
+    _id: string;
+}
+export interface Comment{
+    content:string;
+    dateTime: string;
+    likes: Array<string>
+    parentId2:string;
+    repies:Array;
+    time: string;
+    user:User
+}
