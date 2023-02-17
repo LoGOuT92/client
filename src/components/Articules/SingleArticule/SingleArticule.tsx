27ef99a2e3973905cf338322507392841bc66946
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-02-17 01:17:31 +0100

    fix: fixed class names

--- src/components/Articules/SingleArticule/SingleArticule.tsx
+++ src/components/Articules/SingleArticule/SingleArticule.tsx
@@ -10,18 +10,19 @@ export default function SingleArticule({
   return (
     <article
       className={`${styles.articleContiner} ${
-        smallArticule ? styles.smallArticule : ""
+        smallArticule && styles.smallArticule
       }`}
     >
       <Link href="/post/1640061574985">
-        <div>
+        <div className={styles.articuleImage}>
           <Image
             width={550}
             height={313}
             src="https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/74a26350-377e-45f7-83ed-4a04fa89eab9/2023.02.12-Report-MilanPomigliano-Femminile-Orizzontale.jpg?w=565&auto=format"
-            alt=""
+            alt="Articule image"
           />
         </div>
+
         <h3>TIME MACHINE: AC MILAN AND THE CHAMPIONS LEAGUE ROUND OF 16</h3>
       </Link>
     </article>
